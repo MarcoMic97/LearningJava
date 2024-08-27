09c5563df9ac3e361d3df7a2f1a7322b09d29778
Marco Michalik <marcomichalik@hotmail.dk>
2024-08-27 09:19:37 +0200

    made structural changes

---
+++ LearningJava/src/main/java/com/mycompany/learningjava/Part1.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.learningjava;
+
+public class Part1 {
+    public static void main(String[] args) {
+        System.out.println("Learning Java");
+        System.out.println("234 *26 / 17 is ");
+        System.out.println(234 *26 / 17);
+    }
+   
+}
